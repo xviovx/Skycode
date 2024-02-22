5c2e35fee8f8ae51835dfecf1d7335df85a27ee0
Kyle <pharoahkyle@gmail.com>
2024-02-23 00:28:44 +0200

    routing, nav, and basic styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegistrationComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: UserProfileComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/register', pathMatch: 'full' } // default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
