ab67dcd7e5d26e92d5c4c7c4fc3d1ea5d7e9ffa5
Kyle <pharoahkyle@gmail.com>
2024-02-24 18:07:00 +0200

    adjusted validator logic to ensure password mismatch error displays only when both password fields are filled

--- src/app/components/registration/password-validator.ts
+++ src/app/components/registration/password-validator.ts
@@ -3,5 +3,12 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
     const password = control.get('password')?.value;
     const passwordConfirm = control.get('passwordConfirm')?.value;
-    return password === passwordConfirm ? null : { passwordMismatch: true};
+
+    if (password && passwordConfirm && password !== passwordConfirm) {
+        control.get('passwordConfirm')?.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+    } else if (password && passwordConfirm && password === passwordConfirm) {
+        control.get('passwordConfirm')?.setErrors(null);
+    }
+    return null;
 }
